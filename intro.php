53c2d123e4f1da4fd53bef25d65c78c192734d9d
filbert2008 <ana.rika24@smk.belajar.id>
2025-01-15 15:03:37 +0700

    done

--- intro.php
+++ intro.php
@@ -6,6 +6,32 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+ define( "gaji",5000000);
+ define("bonus",500000);
+ define("pajak",0.12);
+
+ function inputidentitas($nama,$tgsdn){
+      $sblmppn = 0;
+      $ssdhppn = 0;
+      $ppn = 0;
+
+      $sblmppn = bonus * $tgsdn;
+      $sblmppn = $sblmppn + gaji;
+      $ppn = $sblmppn*pajak;
+      $ssdhppn = $sblmppn - ($sblmppn*pajak);
+
+      echo "nama pekerja :". $nama ."<br/>";
+      echo "jumlah tugas(proyek) yang telah selesai :". $tgsdn ."<br/>";
+      echo "pendapatan ". $nama ."keseluruhan adalah : Rp". number_format($ssdhppn)."<br/>";
+      echo "dengan ppn : Rp". number_format($ppn);
+      echo "<br/>";
+      echo "<br/>";
+
+
+ }
+
+ inputidentitas("cimeng",8)
+    ?>
 </body>
 </html>
