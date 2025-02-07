9c8f2bb483093ef97333f2833f93cc1c4ba56bb7
filbert2008 <ana.rika24@smk.belajar.id>
2025-02-07 14:25:36 +0700

    revisi

--- tugas2.php
+++ tugas2.php
@@ -3,185 +3,87 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Pendaftaran</title>
+    <title>form</title>
 </head>
 <body>
-
-  <h2>Form Registrasi SMKN 1 Banyuwangi</h2>
-  <form action="">
+    <form action="" method="">
     <fieldset>
-       <legend>Data Calon Siswa </legend>
+       <legend> <h2>Data Calon Siswa</h2> </legend>
        <p>Nama Calon Siswa : <input type="text" place holder="Nama"></p>
        <p>Tempat Lahir : <input type="text" place holder="Tempat Lahir"></p>
        <p>Jenis Kelamin : <input type="radio" name="jenis_kelamin"Laki-Laki
        <input type="radio" name="jenis_kelamin">perempuan
        </p>
-       <p>Alamat : <input type="text" placeholder="Alamat Anda"></p>
+       <textarea name="Alamat" id=""></textarea>
        <p>Tanggal Lahir : <input type="date"></p>
        <p>NIK : <input type="text" place holder="Nomor Induk Kependudukan"></p>
        <p>NISN : <input type="text" place holder="Nomor Induk Siswa Nasional"></p>
-       <p>Agama : <select name="" id="">
-           <option value="">islam</option>
-           <option value="">Kristen</option>
-           <option value="">Katolik</option>
-           <option value="">Hindu</option>
-           <option value="">Budha</option>
-           <option value="">Konghucu</option>
+       <p>Agama : <select name="Agama Siswa" id="">
+           <option value="islam">islam</option>
+           <option value="Kristen">Kristen</option>
+           <option value="Katolik">Katolik</option>
+           <option value="Hindu">Hindu</option>
+           <option value="Budha">Budha</option>
+           <option value="Konghucu">Konghucu</option>
        </select></p>
-       <p>RT : <input type="text" place holder="Contoh:RT.01"></p>
-       <p>RW : <input type="text" place holder="Contoh:RW.01"></p>
-       <p>Nama Dusun : <input type="text" place holder="Nama Dusun Anda"></p>
-       <p>Nama Kelurahan : <input type="text" place holder="Nama Kelurahan Anda"></p>
-       <p>Nama Kecamatan : <input type="text" place holder="Nama Kecamatan Anda"></p>
+       
        <p>No. Handphone : <input type="tel" place holder="No. Handphone Anda"></p>
-    </fieldset>
-   <br>
-   <br>
-    <fieldset>
-    <legend>Data Ayah </legend>
-       <p>Nama Ayah : <input type="text" place holder="Nama"></p>
-       <p>Tempat Lahir : <input type="text" place holder="Tempat Lahir"></p>
-       <p>Alamat : <input type="text" placeholder="Alamat"></p>
-       <p>Tanggal Lahir : <input type="date"></p>
-       <p>Jenjang pendidikan : <select name="" id="">
-           <option value="">SD</option>
-           <option value="">SMP</option>
-           <option value="">SMA</option>
-           <option value="">S1</option>
-           <option value="">S2</option>
-           <option value="">S3</option>
+   
+   <p>Pilih Jurusan1 : <select name="Jurusan1" id="">
+    <option value="PPLG">PPLG</option>
+    <option value="TJKT">TJKT</option>
+    <option value="DKV">DKV</option>
+    <option value="SP">Seni Pertunjukan</option>
+    <option value="AKEUL">AKL</option>
+    <option value="MPLB">MPLB</option>
+    <option value="PH">Perhotelan</option>
+    <option value="PM">Pemasaran</option>
+    <option value="BDP">BDP</option>
+    <option value="KL">Kuliner</option>
        </select></p>
-       <p>Pekerjaan : <input type="text" place holder"Pekerjaan Ayah"></p>
-       <p>penghasilan per-bulan : <input type="radio" name="Penghasilan_Ayah"> dibawah 1 jt</p>
-       <input type="radio" name="Penghasilan_Ayah"> 1-3 juta </p>
-       <input type="radio" name="Penghasilan_Ayah"> 3-5 juta </p>
-       <input type="radio" name="Penghasilan_Ayah"> 5-10 juta </p>
-       <input type="radio" name="Penghasilan_Ayah"> 10-15 juta </p>
-       <p>NIK : <input type="text" place holder="Nomor Induk Kependudukan"></p>
-       <p>Agama : <select name="" id="">
-           <option value="">islam</option>
-           <option value="">Kristen</option>
-           <option value="">Katolik</option>
-           <option value="">Hindu</option>
-           <option value="">Budha</option>
-           <option value="">Konghucu</option>
+   <p>Pilih Jurusan2 : <select name="Jurusan2" id="">
+          <option value="PPLG">PPLG</option>
+    <option value="TJKT">TJKT</option>
+    <option value="DKV">DKV</option>
+    <option value="SP">Seni Pertunjukan</option>
+    <option value="AKEUL">AKL</option>
+    <option value="MPLB">MPLB</option>
+    <option value="PH">Perhotelan</option>
+    <option value="PM">Pemasaran</option>
+    <option value="BDP">BDP</option>
+    <option value="KL">Kuliner</option>
        </select></p>
-       <p>RT : <input type="text" place holder="Contoh:RT.01"></p>
-       <p>RW : <input type="text" place holder="Contoh:RW.01"></p>
-       <p>Nama Dusun : <input type="text" place holder="Nama Dusun"></p>
-       <p>Nama Kelurahan : <input type="text" place holder="Nama Kelurahan"></p>
-       <p>Nama Kecamatan : <input type="text" place holder="Nama Kecamatan"></p>
-       <p>No. Handphone : <input type="tel" place holder="No. Handphone"></p>
-    </fieldset>
-    <fieldset>
-   <br>
-   <br>
-    <legend>Data Ibu </legend>
-       <p>Nama Ibu : <input type="text" place holder="Nama"></p>
-       <p>Tempat Lahir : <input type="text" place holder="Tempat Lahir"></p>
-       <p>Alamat : <input type="text" placeholder="Alamat"></p>
-       <p>Tanggal Lahir : <input type="date"></p>
-       <p>Jenjang pendidikan : <select name="" id="">
-           <option value="">SD</option>
-           <option value="">SMP</option>
-           <option value="">SMA</option>
-           <option value="">S1</option>
-           <option value="">S2</option>
-           <option value="">S3</option>
+   <p>Pilih Jurusan3 : <select name="Jurusan3" id="">
+           <option value="PPLG">PPLG</option>
+           <option value="TJKT">TJKT</option>
+           <option value="DKV">DKV</option>
+           <option value="SP">Seni Pertunjukan</option>
+           <option value="AKEUL">AKL</option>
+           <option value="MPLB">MPLB</option>
+           <option value="PH">Perhotelan</option>
+           <option value="PM">Pemasaran</option>
+           <option value="BDP">BDP</option>
+           <option value="KL">Kuliner</option>
        </select></p>
-       <p>Pekerjaan : <input type="text" place holder"Pekerjaan Ibu"></p>
-       <p>penghasilan per-bulan : <input type="radio" name="Penghasilan_Ibu"> dibawah 1 jt</p>
-       <input type="radio" name="Penghasilan_Ibu"> 1-3 juta </p>
-       <input type="radio" name="Penghasilan_Ibu"> 3-5 juta </p>
-       <input type="radio" name="Penghasilan_Ibu"> 5-10 juta </p>
-       <input type="radio" name="Penghasilan_Ibu"> 10-15 juta </p>
-       <input type="radio" name="Penghasilan_Ibu"> diatas 15 juta </p>
-       <p>NIK : <input type="text" place holder="Nomor Induk Kependudukan"></p>
-       <p>Agama : <select name="" id="">
-           <option value="">islam</option>
-           <option value="">Kristen</option>
-           <option value="">Katolik</option>
-           <option value="">Hindu</option>
-           <option value="">Budha</option>
-           <option value="">Konghucu</option>
-       </select></p>
-       <p>RT : <input type="text" place holder="Contoh:RT.01"></p>
-       <p>RW : <input type="text" place holder="Contoh:RW.01"></p>
-       <p>Nama Dusun : <input type="text" place holder="Nama Dusun"></p>
-       <p>Nama Kelurahan : <input type="text" place holder="Nama Kelurahan"></p>
-       <p>Nama Kecamatan : <input type="text" place holder="Nama Kecamatan"></p>
-       <p>No. Handphone : <input type="tel" place holder="No. Handphone"></p>
-    </fieldset>
-   <br>
-   <br>
-    <legend>Data Wali </legend>
-       <p>Nama Wali : <input type="text" place holder="Nama"></p>
-       <p>Tempat Lahir : <input type="text" place holder="Tempat Lahir"></p>
-       <p>Alamat : <input type="text" placeholder="Alamat"></p>
-       <p>Tanggal Lahir : <input type="date"></p>
-       <p>Jenjang pendidikan : <select name="" id="">
-           <option value="">SD</option>
-           <option value="">SMP</option>
-           <option value="">SMA</option>
-           <option value="">S1</option>
-           <option value="">S2</option>
-           <option value="">S3</option>
-       </select></p>
-       <p>Pekerjaan : <input type="text" place holder"Pekerjaan Wali"></p>
-       <p>penghasilan per-bulan : <input type="radio" name="Penghasilan_Wali"> dibawah 1 jt</p>
-       <input type="radio" name="Penghasilan_Wali"> 1-3 juta </p>
-       <input type="radio" name="Penghasilan_Wali"> 3-5 juta </p>
-       <input type="radio" name="Penghasilan_Wali"> 5-10 juta </p>
-       <input type="radio" name="Penghasilan_Wali"> 10-15 juta </p>
-       <input type="radio" name="Penghasilan_Wali"> diatas 15 juta </p>
-       <p>NIK : <input type="text" place holder="Nomor Induk Kependudukan"></p>
-       <p>Agama : <select name="" id="">
-           <option value="">islam</option>
-           <option value="">Kristen</option>
-           <option value="">Katolik</option>
-           <option value="">Hindu</option>
-           <option value="">Budha</option>
-           <option value="">Konghucu</option>
-       </select></p>
-       <p>RT : <input type="text" place holder="Contoh:RT.01"></p>
-       <p>RW : <input type="text" place holder="Contoh:RW.01"></p>
-       <p>Nama Dusun : <input type="text" place holder="Nama Dusun"></p>
-       <p>Nama Kelurahan : <input type="text" place holder="Nama Kelurahan"></p>
-       <p>Nama Kecamatan : <input type="text" place holder="Nama Kecamatan"></p>
-       <p>No. Handphone : <input type="tel" place holder="No. Handphone"></p>
-    </fieldset>
-   <p>Pilih Jurusan : <select name="" id="">
-           <option value="">PPLG</option>
-           <option value="">TJKT</option>
-           <option value="">DKV</option>
-           <option value="">Seni Pertunjukan</option>
-           <option value="">AKL</option>
-           <option value="">MPLB</option>
-           <option value="">Perhotelan</option>
-           <option value="">Pemasaran</option>
-           <option value="">BDP</option>
-           <option value="">Kuliner</option>
-       </select></p>
-<p>pilih ekstrakurikuler (optional) : <select name="" id="">
-           <option value="">OSIS</option>
-           <option value="">Dewan Ambalan</option>
-           <option value="">Pecinta Alam</option>
-           <option value="">Remaja Masjid</option>
-           <option value="">Laskar Hijau</option>
-           <option value="">PMR</option>
-           <option value="">Futsal</option>
-           <option value="">Basket</option>
-           <option value="">Voli</option>
-           <option value="">Tari</option>
-           <option value="">Teater Kusuma</option>
-           <option value="">Basket</option>
-           <option value="">Esemkasa Breaking News</option>
-           <option value="">English Club</option>
-       </select></p>
-  <p><input type="checkbox">Saya menyatakan dengan sesungguhnya bahwa isian data di dalam formulir ini adalah benar apabila ternyata data tersebut tidak benar/palsu maka saya bersedia menerima sanksi berupa pembatalan sebagai calon peserta didik.</p>
-
-   </form>
-
-
+<label for="">
+            pilih extra yang diminati
+        </label>
+        <label for="extra1">
+            <input type="checkbox" name="extra[]" id="extra1" value="Pecinta Alam">
+            Pecinta Alam
+        </label>
+        <input type="checkbox" name="extra[]" value="Dewan Ambalan">Dewan Ambalan
+        <input type="checkbox" name="extra[]" value="OSIS">OSIS 
+        <input type="checkbox" name="extra[]" value="Gerakan Disiplin Sekolah">Gerakan Disiplin Sekolah
+        <input type="checkbox" name="extra[]" value="Laskar Hijau">Laskar Hijau 
+        <input type="checkbox" name="extra[]" value=PMR>PMR
+        <input type="checkbox" name="extra[]" value="Remaja Masjid">Remaja Masjid
+        <input type="checkbox" name="extra[]" value="Teater Kusuma">Teater Kusuma
+        <input type="checkbox" name="extra[]" value="Voli">Voli
+        <input type="checkbox" name="extra[]" value="Basket">Basket
+        <input type="checkbox" name="extra[]" value="Futsal">Futsal
+        <input type="checkbox" name="extra[]" value="Paskibra">Paskibra
+        <input type="checkbox" name="extra[]" value="Paskibra">english club
+    </form>
 </body>
 </html>
