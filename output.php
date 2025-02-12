8a510a3437463867ff38cb26bfd09d6440d670dc
filbert2008 <ana.rika24@smk.belajar.id>
2025-02-12 13:21:50 +0700

    add transisi button

--- output.php
+++ output.php
@@ -17,7 +17,7 @@
             margin: 0 auto;
             padding: 20px;
             border-radius: 10px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
         }
         h2 {
             color: #333;
