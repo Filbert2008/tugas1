0e67e4928406c45b90694b3e6704effb0cc3e81e
filbert2008 <ana.rika24@smk.belajar.id>
2025-02-12 13:50:48 +0700

    revisi

--- tugas3.php
+++ tugas3.php
@@ -9,7 +9,7 @@
 <body>
 
     <form action="output.php" method="POST">
-        <legend><h2>Form Penghitungan Gaji Bersih</h2></legend>
+        <legend><h2>Form Penghitung Gaji <span>CV.TeknoNusa</span> </h2></legend>
         <fieldset>
             <label>Nama Karyawan:</label>
             <input type="text" name="nama" required>
